22cb9bbc2bb618bdc0fd74918a45c5da71679446
Stéphane Bagnier <stephane.bagnier@gmail.com>
2014-11-13 16:44:41 +0100

    Cleanup

--- LosTechies.DaysOfRefactoring.SampleCode/Day2_MoveMethod/BankAccount_Before.cs
+++ LosTechies.DaysOfRefactoring.SampleCode/Day2_MoveMethod/BankAccount_Before.cs
@@ -45,37 +45,3 @@ namespace LosTechies.DaysOfRefactoring.MoveMethod.Before
 		}
 	}
 }
-
-namespace LosTechies.DaysOfRefactoring.MoveMethod.After
-{
-	public class AccountInterest
-	{
-		public BankAccount Account { get; private set; }
-
-		public AccountInterest(BankAccount account)
-		{
-			Account = account;
-		}
-
-		public double InterestRate
-		{
-			get { return CalculateInterestRate(); }
-		}
-
-		public bool IntroductoryRate
-		{
-			get { return CalculateInterestRate() < 0.05; }
-		}
-
-		public double CalculateInterestRate()
-		{
-			if (Account.CreditScore > 800)
-				return 0.02;
-
-			if (Account.AccountAge > 10)
-				return 0.03;
-
-			return 0.05;
-		}
-	}
-}
